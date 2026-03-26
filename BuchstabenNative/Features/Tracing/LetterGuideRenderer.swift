ea843a5e634fbea00e09a24dab41b83200d6dbd0
AutoCoder <autocoder@claudebox>
2026-03-26 11:30:45 +0100

    fix: [ios] In 'LetterGuideRenderer.swift', in 'guidePath(for:in:)', add a guard check 'guard rect.width > 0 && rect.height > 0 else

--- BuchstabenNative/Features/Tracing/LetterGuideRenderer.swift
+++ BuchstabenNative/Features/Tracing/LetterGuideRenderer.swift
@@ -6,6 +6,7 @@ import CoreGraphics
 struct LetterGuideRenderer {
     /// Returns a SwiftUI `Path` for the given letter scaled to `rect`.
     static func guidePath(for letter: String, in rect: CGRect) -> Path? {
+        guard rect.width > 0 && rect.height > 0 else { return nil }
         guard let cgPath = LetterGuideGeometry.cgPath(for: letter, in: rect) else {
             return nil
         }
